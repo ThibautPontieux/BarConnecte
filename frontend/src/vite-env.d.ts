8d3ff4da3a5bf17c754a6a45486633936723c85c
Thibaut Pontieux <thibaut.pontieux@gmail.com>
2025-07-27 22:51:01 +0200

    Move API url in env var & docker-compose completion

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+/// <reference types="node" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_ADMIN_URL: string
+  readonly VITE_API_PUBLIC_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api/admin': {
-        target: 'https://localhost:8080', // URL de ton API Admin
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api\/admin/, '/admin')
-      },
-      '/api/public': {
-        target: 'https://localhost:8090', // URL de ton API Public
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api\/public/, '/public')
+export default defineConfig(({ mode }) => {
+  // Charger les variables d'environnement
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api/admin': {
+          target: env.VITE_API_ADMIN_URL || 'http://localhost:8080',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api\/admin/, '/admin')
+        },
+        '/api/public': {
+          target: env.VITE_API_PUBLIC_URL || 'http://localhost:8090',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api\/public/, '/public')
+        }
       }
+    },
+    build: {
+      outDir: 'dist',
+      sourcemap: false,
+      minify: 'terser'
     }
-  }
+  };
 });
